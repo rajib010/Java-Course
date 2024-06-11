4db1cd258bf40c97c090c5478cbea557649be0a8
raJiB <pokhrelrajib016@gmail.com>
2024-06-11 14:14:55 +0545

    serialization and deserialization

---
+++ InputOutput/Serialization/SerializeDemo.java
@@ -0,0 +1,17 @@
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+
+public class SerializeDemo {
+    public static void main(String[] args) {
+        Person person = new Person("John Doe", 30);
+
+        try (FileOutputStream fileOut = new FileOutputStream("person.ser");
+             ObjectOutputStream out = new ObjectOutputStream(fileOut)) {
+            out.writeObject(person);
+            System.out.println("Serialized data is saved in person.ser");
+        } catch (IOException i) {
+            i.printStackTrace();
+        }
+    }
+}
